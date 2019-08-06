b7edcc6fe1a3cb52c5b5a58728e406135481768d
Ricardo Lau <rcrdlau@gmail.com>
2019-08-05 21:21:05 -0300

    pages

---
+++ src/components/Lista.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const c = () => {
+  return (
+    <ul className="lista">
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+    </ul>
+  )
+}
+
+export default c
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Lista from '../components/Lista'
+// import logo from './logo.svg';
+//<img src={logo} className="App-logo" alt="logo" />
+
+const c = () => {
+  return (
+    <>
+      <h1>Olá</h1>
+      <h1>Olá</h1>
+      <Lista></Lista>
+    </>
+  )
+}
+
+export default c
