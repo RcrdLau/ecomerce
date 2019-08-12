c19c5ac7d40c1de0dc1f998e7547ca4c10867dfc
Eric Havel <eric.havel@gmail.com>
2019-08-12 19:31:27 -0300

    lightbox

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,15 +2,13 @@ import React from 'react'
 // import lupa from '../img/lupa.png'
 // import cart from '../img/cart.png'
 
-
-
 const menu = () => {
   return (
     <>
         <section class="container">
             <nav> 
                 <h1>
-                    <a href="www.google.com"> {/* colocar o link da pagina inicial do site, seria index.html ??? */}
+                    <a href="/"> {/* colocar o link da pagina inicial do site, seria index.html ??? */}
                         <img src="#" alt="Logo do Site"/>
                     </a>
                 </h1>
@@ -19,12 +17,7 @@ const menu = () => {
                 </form>
                 <ul className="lista-menu">
                     <li>Acesso</li>
-                    <li>Loja
-                        <ul>
-                            <li className="sub-lista">Jogos</li>
-                            <li className="sub-lista">Ofertas</li>
-                        </ul>
-                    </li>
+                    <li><a href="/jogo">Jogo</a></li>
                 </ul>
                 <hr/>
                 <ul className="lista-auxiliar">
---
+++ src/pages/Jogo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Menu from '../components/Menu'
+
+const Jogo = () => {
+  return (
+    <>
+      <h1>Jogo</h1>
+      <Menu></Menu>
+    </>
+  )
+}
+
+export default Jogo
