fb9475d37d1714dc9a5321c15281764399d79776
Eric Havel <eric.havel@gmail.com>
2019-08-26 20:15:30 -0300

    state

--- src/store/ducks/Jogos.js
+++ src/store/ducks/Jogos.js
@@ -2,16 +2,24 @@ export const reducer =  () => {
     return [
         {
             nome: "Fifa",
-            texto: "Uma nova experiência realista de futebol!"
+            texto: "Uma nova experiência realista de futebol!",
+            banner: false,
+            destaque: true
         },{
             nome: "Battlefield",
-            texto: "Faça parte do caos glorioso da guerra!"
+            texto: "Faça parte do caos glorioso da guerra!",
+            banner: true,
+            destaque: true
         },{
             nome: "Apex Legends",
-            texto: "Descubra um modo de jogo e muito mais!"
+            texto: "Descubra um modo de jogo e muito mais!",
+            banner: false,
+            destaque: true
         },{
             nome: "The Sims",
-            texto: "Descubra um modo de jogo e muito mais!"
+            texto: "Descubra um modo de jogo e muito mais!",
+            banner: false,
+            destaque: false
         }
     ]
 }
